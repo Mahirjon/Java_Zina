59ad3abe021b66c999298c8d9794bfe740f03f75
Mohir <Mohir@fdf.re>
2022-05-09 22:43:41 +0500

    dz-3

---
+++ script .js
@@ -0,0 +1,12 @@
+let number = +prompt('Son kiriting!') 
+let daraja = +prompt('Son darajsini kiriting!')
+
+answer = 1
+
+for( i = 0; i < daraja; i++){
+    answer = answer * number
+}
+alert(answer)
+
+
+
---
+++ script2.js
@@ -0,0 +1,40 @@
+let number = +prompt('Zinalar qatori')
+let soni = prompt('Qatorlar belgisi ')
+let belgi = prompt('Oxirgi belgi')
+
+
+for( let i = 0; i < number ; i++){
+
+  
+      /* ozgaruvchi */ /* shart */   /* true qaytarsa nima bolishi */  /* false qaytarsa nima bolishi */  
+
+   /* number = !isNAN(number) && number > 0 ? number : number = 6   */
+
+//    belgi = i == 0  ? belgi : soni + belgi
+//    console.info(belgi)
+
+    console.info(belgi)
+    belgi = soni + belgi
+}
+
+
+
+/* 
+
+
+*
+/*
+//*
+///*
+///*
+////*
+
+ */
+
+
+
+
+
+
+
+
